887d0e835df6fd4954b57d6bbfdcc1d10126b54b
岩橋拓海 <minton@kin.local>
2023-10-04 17:32:40 +0900

    add/UserモデルとJob_entriesモデルの関連付けを追加
    userモデルのuser_idとjob_entriesモデルのupdate_byカラムを関連づけ

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -1,3 +1,4 @@
 class JobEntry < ApplicationRecord
+  belongs_to :user, foreign_key: 'update_by'
   belongs_to :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :contacts, foreign_key: 'update_by'
+  has_many :job_entries, foreign_key: 'update_by'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

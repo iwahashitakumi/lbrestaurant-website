d05ef7ec16cfc1e6ab74a0e0a240b08803db9cd0
岩橋拓海 <minton@kin.local>
2023-11-07 16:39:55 +0900

    add/開発環境メール送信の確認をできるように、gemを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   namespace :users do
     root 'home#show'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end

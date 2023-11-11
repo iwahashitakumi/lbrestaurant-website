248bcca7c9b7178f68f756de70b7b680fbf12eb2
岩橋拓海 <minton@kin.local>
2023-11-11 16:58:56 +0900

    fix/管理側のapplicationコントローラーにbefore_action :configure_permitted_parametersうつしてしまったため、エラーが起き名前などが編集できなくなったため、元の位置に戻した

--- app/controllers/admins/application_controller.rb
+++ app/controllers/admins/application_controller.rb
@@ -1,6 +1,6 @@
 class Admins::ApplicationController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_admin!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+ 
 
 end

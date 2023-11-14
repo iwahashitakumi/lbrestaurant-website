91abb456797ee24b0d5ae759da75a6a57292052e
岩橋拓海 <minton@kin.local>
2023-11-14 16:50:35 +0900

    update/サイドバーにお知らせ新規作成と一覧ページへのリンクを追加し不要なpropsを削除

--- app/frontend/src/Sidebar.tsx
+++ app/frontend/src/Sidebar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 interface SidebarProps {
+  adminsNewsIndexPath: string;
+  newAdminsNewsPath: string;
   isSidebarOpen: boolean;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen }) => {
-  console.log(`[Sidebar]isSidebarOpenの値は${isSidebarOpen}です。`);
+const Sidebar: React.FC<SidebarProps> = (props) => {
+  console.log(`[Sidebar]isSidebarOpenの値は${props.isSidebarOpen}です。`);
   return (
-    <nav id="sidebar" className={`col-md-3 col-lg-2 bg-light sidebar `} style={{display: isSidebarOpen ? 'block' : 'none'}}>
+    <nav id="sidebar" className={`col-md-3 col-lg-2 bg-light sidebar `} style={{display: props.isSidebarOpen ? 'block' : 'none'}}>
       <div className="flex-shrink-0 p-3 bg-dark" style={{ height: '100%' }}>
         <a href="/" className="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
           <span className="fs-5 fw-semibold text-white mx-auto">ダッシュボード</span>
@@ -15,12 +17,12 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen }) => {
         <ul className="list-unstyled ps-0">
           <li className="mb-1">
             <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 text-white" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-              ホーム
+              お知らせ
             </button>
             <div className="collapse show" id="home-collapse">
               <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">概要</a></li>
-                <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">更新</a></li>
+                <li><a href={props.adminsNewsIndexPath} className="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">一覧</a></li>
+                <li><a href={props.newAdminsNewsPath} className="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">新規作成</a></li>
                 <li><a href="#" className="link-body-emphasis d-inline-flex text-decoration-none rounded text-white">レポート</a></li>
               </ul>
             </div>

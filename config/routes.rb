b6c23226cab470d070e74252c6f865fa11d2cc4d
岩橋拓海 <minton@kin.local>
2023-11-14 16:24:54 +0900

    update/newsテーブルのルートを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   devise_scope :admin do
     get '/admins/sign_out' => 'devise/sessions#destroy'
   end
-  namespace :admins do
-    root 'home#show'
-  end
+  
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+  namespace :admins do
+    root 'home#show'
+    resources :news
   end
 end

333c919632ac9bb842bcc073efcf516d70c529f0
岩橋拓海 <minton@kin.local>
2023-11-17 05:40:40 +0900

    update/sessionでurlを保存し編集や新規作成をした後も検索後のページが表示される機能の作成

--- app/controllers/admins/news_controller.rb
+++ app/controllers/admins/news_controller.rb
@@ -3,17 +3,20 @@ class Admins::NewsController < Admins::ApplicationController
     @q = News.ransack(params[:q])
     @news = @q.result(distinct: true).page(params[:page])
     @search_scope = :title_or_body_cont
+    session[:news_index_url] = request.url
   end
 
   def new
     @news = News.new
+    @news_index_url = session[:news_index_url]
   end
-
+  
   def create
     @news = News.new(news_params)
+    @news_index_url = session[:news_index_url]
     begin
       @news.save!
-      redirect_to :admins_news_index, notice: "お知らせの投稿ができました"
+      redirect_to @news_index_url, notice: "お知らせの投稿ができました"
     rescue
       render "new", alert: "お知らせの投稿ができませんでした"
     end
@@ -21,27 +24,31 @@ class Admins::NewsController < Admins::ApplicationController
   
   def show
     @news = News.find(params[:id])
+    @news_index_url = session[:news_index_url]
   end
-
+  
   def edit
     @news = News.find(params[:id])
+    @news_index_url = session[:news_index_url]
   end
-
+  
   def update
     @news = News.find(params[:id])
+    @news_index_url = session[:news_index_url]
     begin
       @news.update!(news_params)
-      redirect_to :admins_news_index, notice: "お知らせの内容を変更できました"
+      redirect_to @news_index_url, notice: "お知らせの内容を変更できました"
     rescue
       render 'edit', alert: "お知らせの内容を変更できませんでした"
     end
   end
-
+  
   def destroy
     @news = News.find(params[:id])
     @news.destroy
-    redirect_to :admins_news_index, notice: "お知らせを削除しました"
+    redirect_to @news_index_url|| admins_news_index_path, notice: "お知らせを削除しました"
   end
+  
 
   private
   

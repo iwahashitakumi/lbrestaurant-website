ea124c67666deacf9256921cc1d21ff0f4e41732
岩橋拓海 <minton@kin.local>
2023-11-17 02:28:57 +0900

    update/ransackable_attributesとransackable_associationsメソッドを追加
    ransackable_attributesメソッドでは、Newsモデルの検索可能な属性を指定しています。ransackable_associationsメソッドでは、関連がないため空の配列を返しています。

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,9 @@
 class News < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["title", "body"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end

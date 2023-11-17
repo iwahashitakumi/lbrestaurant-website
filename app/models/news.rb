83f91e5686e48e751927b3bb63dd279cb7f1e24d
岩橋拓海 <minton@kin.local>
2023-11-18 00:34:43 +0900

    update/newsカラムにvalidatesを追加

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,20 @@
 class News < ApplicationRecord
+  validates :calendar_date, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validates :body, presence: true
+  validate :start_at_in_future
+  validate :end_at_after_start_at
+
+  def start_at_in_future
+    errors.add(:start_at, 'は過去の日時を選択できません') if start_at.present? && start_at < Time.current
+  end
+
+  def end_at_after_start_at
+    errors.add(:end_at, 'は開始日時より後の日時を選択してください') if start_at.present? && end_at.present? && end_at <= start_at
+  end
+  
   def self.ransackable_attributes(auth_object = nil)
     ["title", "body"]
   end

ff79dae53a92d1b942d3e1c7de20b0f2c40fe817
岩橋拓海 <minton@kin.local>
2023-11-22 20:16:47 +0900

    update/デフォルトのfield_with_errorsによりレイアウトが崩れるので非表示になるよう設定

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module LbrestaurantWebsite
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      html_tag.html_safe
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

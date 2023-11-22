b18e4a9a8c825f4a5f0237c344fbb537718f7c0a
岩橋拓海 <minton@kin.local>
2023-11-22 21:01:44 +0900

    add/店舗編集ページとcontrollerの設定を作成

--- app/controllers/admins/shops_controller.rb
+++ app/controllers/admins/shops_controller.rb
@@ -17,7 +17,25 @@ class Admins::ShopsController < Admins::ApplicationController
       render "new"
     end
   end
+
+  def edit
+    @shop = Shop.find(params[:id])
+    @prefectures = Prefecture.all
+    @shops_index_url = session[:shops_index_url]
+  end
   
+  def update
+    @shops = Shops.find(params[:id])
+    @prefectures = Prefecture.all
+    @shops_index_url = session[:shops_index_url]
+    begin
+      @shop.update!(shop_params)
+      redirect_to @shops_index_url, notice: "店舗の内容を変更できました"
+    rescue
+      render 'edit', alert: "店舗の内容を変更できませんでした"
+    end
+  end
+
   private
   
   def shop_params

a479755418171b16cd7b174ecd2a20fb00bb0e5e
岩橋拓海 <minton@kin.local>
2023-11-26 20:16:57 +0900

    add/carrierwave-gemを利用してarticleモデルのカラム用にアップローダーファイルを追加しモデルに設定を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  mount_uploader :article_images, ArticleImagesUploader
   extend Enumerize
 
   enumerize :category, in: { event: 0, company_trip: 1, staff_introduction: 2, other: 3 }

7bdaffb04a84d59c9b5b1ff79365b05b68ab3608
岩橋拓海 <minton@kin.local>
2023-11-27 20:05:10 +0900

    update/shopモデルの削除された店舗一連と復元機能のリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,27 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
     registrations: 'admins/registrations'
   }
+
   devise_scope :admin do
     get '/admins/sign_out' => 'devise/sessions#destroy'
   end
-  
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
   namespace :admins do
     root 'home#show'
     resources :news
-    resources :shops
+    resources :shops do
+      member do
+        post :restore, to: 'shops#restore'
+      end
+
+      collection do
+        get :discarded, to: 'shops#discarded'
+      end
+    end
     resources :articles
   end
 end

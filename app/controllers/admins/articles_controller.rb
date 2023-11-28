3850916b73213579155b4eabc3b000e3129bf98c
岩橋拓海 <minton@kin.local>
2023-11-28 16:30:02 +0900

    add/articleモデルの一覧ページとcontrollerの設定

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -1,2 +1,21 @@
 class Admins::ArticlesController < Admins::ApplicationController
+  def index
+    @q = Article.ransack(params[:q])
+    @articles = @q.result(distinct: true).page(params[:page])
+    session[:articles_index_url] = request.url
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :start_at, :end_at, :category, sets: [:body, :article_images])
+  end  
+
+  def start_at_options
+    {  min: Time.zone.now.strftime("%Y-%m-%dT%H:%M"), max: (Time.zone.now + 1.year), value: Time.zone.now.strftime("%Y-%m-%dT%H:%M") }
+  end
+
+  def end_at_options
+    { min: (Time.zone.now + 1.day).strftime("%Y-%m-%dT%H:%M"), value: (Time.zone.now + 1.day).strftime("%Y-%m-%dT%H:%M") }
+  end
 end

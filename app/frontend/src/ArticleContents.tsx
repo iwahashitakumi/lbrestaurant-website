4ae825be8cd13fb2d5d49713872b7f0d5e0a4794
岩橋拓海 <minton@kin.local>
2023-12-01 17:57:53 +0900

    update/プラスボタンやマイナスボタンでcontentsを増やしり削除したりできるように設定

--- app/frontend/src/ArticleContents.tsx
+++ app/frontend/src/ArticleContents.tsx
@@ -1,126 +1,93 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-interface ContentsFormProps {
-  index: number;
-  totalContents: number;
-  content: {
-    body: string;
-    article_images: File | null;
-    article_images_cache: string;
-    errors: { body?: string[]; article_images?: string[] };
-  };
-  onRemove: () => void;
-  onChange: (key: string, value: string | File | null) => void;
+interface Content {
+  body: string;
+  article_images: { url: string | null };
+  article_images_cache: string;
+  _destroy?: boolean;
 }
 
-const ContentsForm: React.FC<ContentsFormProps> = ({ index, totalContents, content, onRemove, onChange }) => {
-  const handleInputChange = (key: string, e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(key, e.target.value);
-  };
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0] || null;
-    onChange('article_images', file);
-  };
-
-  return (
-    <div className="mb-4">
-      <label htmlFor={`contents_${index}_body`} className="form-label">
-        内容{index + 1}
-      </label>
-      <span className="badge rounded-pill text-bg-danger">必須</span>
-      <textarea
-        id={`contents_${index}_body`}
-        name={`article[contents_attributes][${index}][body]`}
-        value={content.body}
-        onChange={(e) => handleInputChange('body', e)}
-        className="form-control"
-      />
-      
-      {content.errors.body && content.errors.body.map((error, errorIndex) => (
-        <div key={`body_error_${index}_${errorIndex}`} className="text-danger">
-          {error}
-        </div>
-      ))}
-
-      <label htmlFor={`contents_${index}_article_images`} className="form-label">
-        画像{index + 1}
-      </label>
-      <span className="badge rounded-pill text-bg-success">任意</span>
-      <input
-        type="file"
-        id={`contents_${index}_article_images`}
-        name={`article[contents_attributes][${index}][article_images]`}
-        onChange={handleFileChange}
-        className="form-control"
-        accept="image/*"
-      />
-      <input
-        type="hidden"
-        id={`contents_${index}_article_images_cache`}
-        name={`article[contents_attributes][${index}][article_images_cache]`}
-        value={content.article_images_cache || ''}
-      />
-      {content.errors.article_images && content.errors.article_images.map((error, errorIndex) => (
-        <div key={`article_images_error_${index}_${errorIndex}`} className="text-danger">
-          {error}
-        </div>
-      ))}
-
-      {totalContents > 1 && index === totalContents - 1 && (
-        <button type="button" onClick={onRemove} className="btn btn-danger">
-          -
-        </button>
-      )}
-    </div>
-  );
-};
+interface Article {
+  contents: Content[];
+}
 
 const ArticleContents: React.FC = () => {
-  const initialContent = {
-    body: '',
-    article_images: null,
-    article_images_cache: '',
-    errors: {},
-  };
+  const [contents, setContents] = useState<Article['contents']>([]);
 
-  const [contents, contentContents] = useState([initialContent]);
+  useEffect(() => {
+    const element = document.querySelector('[data-props]');
+    const dataProps = element?.getAttribute('data-props');
+
+    if (dataProps) {
+      const parsedData = JSON.parse(dataProps);
+      setContents(parsedData.contents || [{ body: '', article_images: { url: null }, article_images_cache: '' }]);
+    }
+  }, []);
 
   const handleAddContent = () => {
-    contentContents([...contents, initialContent]);
+    setContents((prevContents) => [
+      ...prevContents,
+      { body: '', article_images: { url: null }, article_images_cache: '' },
+    ]);
   };
 
   const handleRemoveContent = (index: number) => {
-    if (index === 0 && contents.length === 1) {
-      return;
+    if (contents.length > 1) {
+      setContents((prevContents) => {
+        const newContents = [...prevContents];
+        newContents.splice(index, 1);
+        return newContents;
+      });
     }
-    const newContents = [...contents];
-    newContents.splice(index, 1);
-    contentContents(newContents);
   };
 
-  const handleContentChange = (index: number, key: string, value: string | File | null) => {
-    const newContents = [...contents];
-    newContents[index] = { ...newContents[index], [key]: value, errors: {} };
-    contentContents(newContents);
+  const handleChange = (index: number, field: keyof Content, value: any) => {
+    setContents((prevContents) => {
+      const newContents = [...prevContents];
+      newContents[index][field] = value;
+      return newContents;
+    });
   };
 
   return (
-    <form>
+    <div>
       {contents.map((content, index) => (
-        <ContentsForm
-          key={index}
-          index={index}
-          totalContents={contents.length}
-          content={content}
-          onRemove={() => handleRemoveContent(index)}
-          onChange={(key, value) => handleContentChange(index, key, value)}
-        />
+        <div key={index} style={{ marginBottom: '10px' }}>
+          <>
+            <label className="form-label">内容</label>
+            <span className="badge rounded-pill text-bg-danger">必須</span>
+            <input
+              type="text"
+              className="form-control"
+              value={content.body}
+              placeholder="内容を入力"
+              onChange={(e) => handleChange(index, 'body', e.target.value)}
+              name={`article[contents_attributes][${index}][body]`}
+            />
+            
+            <label className="form-label">画像</label>
+            <span className="badge rounded-pill text-bg-success">任意</span>
+            <input
+              type="file"
+              className="form-control"
+              accept="image/*"
+              onChange={(e) => handleChange(index, 'article_images', e.target.files)}
+              name={`article[contents_attributes][${index}][article_images]`}
+            />
+            <input type="hidden" value={content.article_images_cache} name={`article[contents_attributes][${index}][article_images_cache]`} />
+            <span className="text-muted small mt-2">・5MBまでの画像をアップロードできます。</span>
+            {contents.length > 1 && index === contents.length - 1 && (
+              <button type="button" onClick={() => handleRemoveContent(index)} className="btn btn-danger" style={{ marginLeft: '10px' }}>
+                -
+              </button>
+            )}
+          </>
+        </div>
       ))}
       <button type="button" onClick={handleAddContent} className="btn btn-primary">
         +
       </button>
-    </form>
+    </div>
   );
 };
 

978c44e8fb7223074c8544d5118f0d6d6f5a3cfc
岩橋拓海 <minton@kin.local>
2023-12-01 18:56:52 +0900

    clean/shopモデルのcontrollerでプライベートメソッドとbefore_actionを利用して各アクションの可読性を向上

--- app/controllers/admins/shops_controller.rb
+++ app/controllers/admins/shops_controller.rb
@@ -1,4 +1,8 @@
 class Admins::ShopsController < Admins::ApplicationController
+  before_action :set_shops_index_url, only: [:discarded, :new, :create, :show, :edit, :update, :destroy, :restore]
+  before_action :set_shops_discarded_url, only: [:show]
+  before_action :set_shop, only: [:show, :edit, :update, :destroy, :restore]
+
   def index
     @q = Shop.kept.ransack(params[:q])
     @shops = @q.result(distinct: true).page(params[:page])
@@ -10,7 +14,6 @@ class Admins::ShopsController < Admins::ApplicationController
     @q = Shop.discarded.ransack(params[:q])
     @discarded_shops = @q.result(distinct: true).page(params[:page])
     @search_residence_scope = :prefecture_name_or_city_name_or_address_cont
-    @shops_index_url = session[:shops_index_url]
     @is_discarded = true
     session[:shops_discarded_url] = request.url
   end
@@ -18,13 +21,11 @@ class Admins::ShopsController < Admins::ApplicationController
   def new
     @shop = Shop.new
     @prefectures = Prefecture.all
-    @shops_index_url = session[:shops_index_url]
   end
   
   def create
     @shop = Shop.new(shop_params)
     @prefectures = Prefecture.all
-    @shops_index_url = session[:shops_index_url]
     begin
       @shop.save!
       redirect_to @shops_index_url, notice: "店舗の登録ができました"
@@ -35,22 +36,16 @@ class Admins::ShopsController < Admins::ApplicationController
   end
   
   def show
-    @shop = Shop.find(params[:id])
-    @shops_index_url = session[:shops_index_url]
     @shops_discarded_url = session[:shops_discarded_url]
     @is_discarded = params[:is_discarded].present?
   end
 
   def edit
-    @shop = Shop.find(params[:id])
     @prefectures = Prefecture.all
-    @shops_index_url = session[:shops_index_url]
   end
   
   def update
-    @shop = Shop.find(params[:id])
     @prefectures = Prefecture.all
-    @shops_index_url = session[:shops_index_url]
     begin
       @shop.update!(shop_params)
       redirect_to @shops_index_url, notice: "店舗の内容を変更できました"
@@ -61,8 +56,6 @@ class Admins::ShopsController < Admins::ApplicationController
   end
   
   def destroy
-    @shop = Shop.find(params[:id])
-    @shops_index_url = session[:shops_index_url]
     begin
       @shop.discard!
       redirect_to @shops_index_url|| admins_shops_path, notice: "店舗を削除しました"
@@ -73,8 +66,6 @@ class Admins::ShopsController < Admins::ApplicationController
   end
 
   def restore
-    @shop = Shop.find(params[:id])
-    @shops_index_url = session[:shops_index_url]
     begin
       @shop.undiscard!
       redirect_to @shops_index_url, notice: "店舗を元に戻しました"
@@ -89,5 +80,17 @@ class Admins::ShopsController < Admins::ApplicationController
   
   def shop_params
     params.require(:shop).permit(:name, :address, :access, :business_time, :phone_number, :counter_seat, :table_seat, :site_name, :gourmet_site_link, :city_name, :shop_image,:shop_image_cache, :prefecture_id)
+  end
+  
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def set_shops_index_url
+    @shops_index_url = session[:shops_index_url]
+  end
+  
+  def set_shops_discarded_url
+    @shops_discarded_url = session[:shops_discarded_url]
   end
 end

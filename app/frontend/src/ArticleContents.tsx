f025d1e752232f5d013d58bda2f444cd480843a3
岩橋拓海 <minton@kin.local>
2023-12-04 20:53:49 +0900

    update/_destroyならcontentを非表示にし、rails側で削除されるコンテンツを非表示にする

--- app/frontend/src/ArticleContents.tsx
+++ app/frontend/src/ArticleContents.tsx
@@ -78,48 +78,57 @@ const ArticleContents: React.FC<ArticleContentsProps> = (props) => {
             </>
           )}
 
-          <label className="form-label">内容</label>
-          <span className="badge rounded-pill text-bg-danger">必須</span>
-          <input
-            type="text"
-            className="form-control"
-            value={content.body}
-            placeholder="内容を入力"
-            onChange={(e) => handleChange(index, 'body', e.target.value)}
-            name={`article[contents_attributes][${index}][body]`}
-          />
-          {content.body === '' && (
-            <div className="text-danger">
-              <ul className="mb-0">
-                <li>{props.body_error_message}</li>
-              </ul>
-            </div>
-          )}
+          {content._destroy ? (
+            <>
+              <input type="hidden" name={`article[contents_attributes][${index}][_destroy]`} value="true" />
+              <input type="hidden" name={`article[contents_attributes][${index}][id]`} value={content.id} />
+            </>
+          ) : (
+            <>
+              <label className="form-label">内容</label>
+              <span className="badge rounded-pill text-bg-danger">必須</span>
+              <input
+                type="text"
+                className="form-control"
+                value={content.body}
+                placeholder="内容を入力"
+                onChange={(e) => handleChange(index, 'body', e.target.value)}
+                name={`article[contents_attributes][${index}][body]`}
+              />
+              {content.body === '' && (
+                <div className="text-danger">
+                  <ul className="mb-0">
+                    <li>{props.body_error_message}</li>
+                  </ul>
+                </div>
+              )}
 
-          <label className="form-label">画像</label>
-          <span className="badge rounded-pill text-bg-success">任意</span>
-          <input
-            type="file"
-            className="form-control"
-            accept="image/*"
-            onChange={(e) => {
-              handleChange(index, 'article_images', e.target.files);
-            }}
-            name={`article[contents_attributes][${index}][article_images]`}
-          />
+              <label className="form-label">画像</label>
+              <span className="badge rounded-pill text-bg-success">任意</span>
+              <input
+                type="file"
+                className="form-control"
+                accept="image/*"
+                onChange={(e) => {
+                  handleChange(index, 'article_images', e.target.files);
+                }}
+                name={`article[contents_attributes][${index}][article_images]`}
+              />
 
-          <input
-            type="hidden"
-            value={content.article_images_cache}
-            name={`article[contents_attributes][${index}][article_images_cache]`}
-          />
-        
-          {contents.length > 1 && (
-            <button type="button" onClick={() => handleRemoveContent(index)} className="btn btn-danger">
-              -
-            </button>
+              <input
+                type="hidden"
+                value={content.article_images_cache}
+                name={`article[contents_attributes][${index}][article_images_cache]`}
+              />
+              
+              {contents.length > 1 && (
+                <button type="button" onClick={() => handleRemoveContent(index)} className="btn btn-danger">
+                  -
+                </button>
+              )}
+              <span className="text-muted small mt-2">・5MBまでの画像をアップロードできます。</span>
+            </>
           )}
-          <span className="text-muted small mt-2">・5MBまでの画像をアップロードできます。</span>
         </div>
       ))}
 

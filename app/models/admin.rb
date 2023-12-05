47a9f549e292099c8c5dc89542bbe19b15bc807b
岩橋拓海 <minton@kin.local>
2023-12-05 21:58:38 +0900

    update/enumerizeを利用してadminモデルのrole列挙型を追加しvalidatesを追加

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,12 @@
 class Admin < ApplicationRecord
   has_many :contacts, foreign_key: 'update_by'
   has_many :job_entries, foreign_key: 'update_by'
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+
+  enumerize :role, in: { member: 0, admin: 1 }, default: :member, predicates: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name,
   length: { minimum: 2, maximum: 20 }
+  validates :role, presence: true
 end

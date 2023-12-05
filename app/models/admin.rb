b0261f1fdcc8fa17bd7c279984a06836451721ba
岩橋拓海 <minton@kin.local>
2023-12-05 22:19:30 +0900

    fix/extend Enumerizeを失念していたため、enumerizeを利用できなかったので追加

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   has_many :contacts, foreign_key: 'update_by'
   has_many :job_entries, foreign_key: 'update_by'
   
-
+  extend Enumerize
   enumerize :role, in: { member: 0, admin: 1 }, default: :member, predicates: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

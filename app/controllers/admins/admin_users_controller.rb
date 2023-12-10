a384f9566f39bc1b7bb5d473a5a9fae2979931c0
岩橋拓海 <minton@kin.local>
2023-12-10 15:17:21 +0900

    update/editable_owner_admin_user!メソッドの式をreturnifを利用する式に変更し、認証情報をbefore_actionではなく各メソッドで直接呼び出すように設定

--- app/controllers/admins/admin_users_controller.rb
+++ app/controllers/admins/admin_users_controller.rb
@@ -1,20 +1,21 @@
 class Admins::AdminUsersController < Admins::ApplicationController
   before_action :assign_admin_user, only: [:show, :edit, :update, :destroy]
   before_action :assign_admin_users_index_url, only: [:new, :create, :show, :edit, :update, :destroy]
-  before_action :authenticate_role!
-  before_action :authenticate_owner!, only: [:show, :edit, :update, :destroy]
 
   def index
+    authenticate_role!
     @q = Admin.ransack(params[:q])
     @admin_users = @q.result(distinct: true).page(params[:page])
     session[:admin_users_index_url] = request.url
   end
   
   def new
+    authenticate_role!
     @admin_user = Admin.new
   end
   
   def create
+    authenticate_role!
     @admin_user = Admin.new(admin_user_params)
     begin
       @admin_user.save!
@@ -26,12 +27,17 @@ class Admins::AdminUsersController < Admins::ApplicationController
   end
 
   def show
+    authenticate_role!
   end
   
   def edit
+    authenticate_role!
+    editable_owner_admin_user!
   end
   
   def update
+    authenticate_role!
+    editable_owner_admin_user!
     begin
       @admin_user.update!(admin_user_params)
       redirect_to @admin_users_index_url, notice: "#{@admin_user.name}の内容を変更できました"
@@ -42,6 +48,8 @@ class Admins::AdminUsersController < Admins::ApplicationController
   end
 
   def destroy
+    authenticate_role!
+    editable_owner_admin_user!
     begin
       @admin_user.destroy!
       redirect_to @admin_users_index_url|| admins_admin_users_path, notice: "#{@admin_user.name}を削除しました"
@@ -65,11 +73,11 @@ class Admins::AdminUsersController < Admins::ApplicationController
     @admin_users_index_url = session[:admin_users_index_url]
   end
 
-  def authenticate_owner!
-    if @admin_user.role == :owner && current_admin.role != :owner
-      flash[:alert] = "オーナー情報を編集する権限がありません。"
-      redirect_to admins_admin_users_path
-    end
+  def editable_owner_admin_user!
+    return if current_admin.role.owner?
+    return unless @admin_user.role.owner?
+    flash[:alert] = "オーナー情報を編集する権限がありません。"
+     redirect_to admins_admin_users_path
   end
 
   def authenticate_role!

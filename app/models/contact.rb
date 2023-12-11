28d959efb92dfda0c9a889c53bbb65c87afc9167
岩橋拓海 <minton@kin.local>
2023-12-11 23:46:43 +0900

    update/contactモデルのstatus列挙型の未対応なものについて、語を変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,5 +3,5 @@ class Contact < ApplicationRecord
 
   extend Enumerize
   enumerize :contact_type, in: { store_service: 0, business_negotiations: 1, job_entry: 2, franchise: 3, other: 4 }, scope: true, predicates: true
-  enumerize :status, in: {pending: 0, in_progress: 1, completed: 2}, default: :pending, scope: true, predicates: true
+  enumerize :status, in: {not_started: 0, in_progress: 1, completed: 2}, default: :pending, scope: true, predicates: true
 end

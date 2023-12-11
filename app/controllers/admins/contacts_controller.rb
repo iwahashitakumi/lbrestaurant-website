3ee0fe785be998992579892d293238c669902fd2
岩橋拓海 <minton@kin.local>
2023-12-12 02:58:06 +0900

    update/contactモデルのcontrollerで、Strong Parametersメソッドを作成

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -1,2 +1,9 @@
 class Admins::ContactsController < Admins::ApplicationController
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:contact_type, :message, :name, :name_kana, :email, :phone_number, :callback_requested, :contact_method, :status).merge(update_by: current_admin.id)
+  end
+  
 end

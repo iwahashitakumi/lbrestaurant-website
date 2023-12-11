06dd177d433aa2a9b1be12f9f13e83dbef5c490b
岩橋拓海 <minton@kin.local>
2023-12-11 23:22:14 +0900

    update/contactモデルのcontact_type列挙型の採用に関する事について、語を変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,6 @@ class Contact < ApplicationRecord
   belongs_to :admin, foreign_key: 'update_by'
 
   extend Enumerize
-  enumerize :contact_type, in: { store_service: 0, business_negotiations: 1, reqruit: 2, franchise: 3, other: 4 }, scope: true, predicates: true
+  enumerize :contact_type, in: { store_service: 0, business_negotiations: 1, job_entry: 2, franchise: 3, other: 4 }, scope: true, predicates: true
   enumerize :status, in: {pending: 0, in_progress: 1, completed: 2}, default: :pending, scope: true, predicates: true
 end

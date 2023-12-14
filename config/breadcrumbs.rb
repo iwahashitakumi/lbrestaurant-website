6ab85b78dd025f0c65af8b938f1a4dc007712f99
岩橋拓海 <minton@kin.local>
2023-12-14 20:29:15 +0900

    update/お知らせの各種ファイルにパンくずリストを作成し、breadcrumbs.rbで設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -16,3 +16,28 @@ crumb :edit_admins_admin_user do
   link "管理ユーザー編集", edit_admins_admin_user_path
   parent :admins_admin_users
 end
+
+crumb :admins_admin_user do |admin_user|
+  link "#{admin_user.name}さんの詳細", admins_admin_user_path(admin_user)
+  parent :admins_admin_users
+end
+
+crumb :admins_news_index do
+  link "お知らせ一覧", admins_news_index_path
+  parent :admins_root
+end
+
+crumb :new_admins_news do
+  link "お知らせ登録", new_admins_news_path
+  parent :admins_news_index
+end
+
+crumb :edit_admins_news do
+  link "お知らせ編集", edit_admins_news_path
+  parent :admins_news_index
+end
+
+crumb :admins_news do |news|
+  link "タイトル:#{news.title}の詳細", admins_news_path(news)
+  parent :admins_news_index
+end

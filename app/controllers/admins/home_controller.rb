8b8b1ebf88a4009e61652c7a15378bd5d8af0ed1
岩橋拓海 <minton@kin.local>
2023-12-16 13:51:03 +0900

    update/管理画面のトップ画面にブログ投稿以外の機能をcardで表示し、それぞれcontrollerでsizeメソッドを使い総数を変数に入れてcardでそれを表示

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -1,4 +1,10 @@
 class Admins::HomeController < Admins::ApplicationController
   def show
+    @contacts = Contact.all
+    @total_contacts = @contacts.size
+    @shops = Shop.all
+    @total_shops = @shops.size
+    @news = News.all
+    @total_news = @news.size
   end
 end

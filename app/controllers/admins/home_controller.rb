6b578d37fa3083d11cfff7adfb4858687fc35e1b
岩橋拓海 <minton@kin.local>
2023-12-16 13:56:42 +0900

    update/管理画面のトップ画面で、本日の問い合わせ件数と応募件数をの総数を表示するようにcontroller側で変数を作成

--- app/controllers/admins/home_controller.rb
+++ app/controllers/admins/home_controller.rb
@@ -2,9 +2,17 @@ class Admins::HomeController < Admins::ApplicationController
   def show
     @contacts = Contact.all
     @total_contacts = @contacts.size
+    @total_contacts_created_today = Contact.where(created_today).size
     @shops = Shop.all
     @total_shops = @shops.size
     @news = News.all
     @total_news = @news.size
   end
+
+  private
+
+  def created_today
+    ["created_at >= ?", Time.zone.now.beginning_of_day]
+  end
+
 end

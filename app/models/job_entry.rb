c2e0f850ab6a19f6f049479b50e6438755543446
岩橋拓海 <minton@kin.local>
2023-12-17 23:47:31 +0900

    update/job_entryモデルで、enumerizeを利用したransackでソート機能を作成するため、スコープを作成

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -8,6 +8,14 @@ class JobEntry < ApplicationRecord
   enumerize :sex, in: {male: 0, female: 1}, scope: true, predicates: true
   enumerize :status, in: {not_started: 0, in_progress: 1, completed: 2}, default: :not_started, scope: true, predicates: true
 
+  scope :search_by_recruit_type, ->(type) { where(recruit_type: type) }
+  scope :search_by_sex, ->(method) { where(sex: method) }
+  scope :search_by_status, ->(status) { where(status: status) }
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i(search_by_recruit_type search_by_sex search_by_status)
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["name","city_name", "address", "recruit_type", "sex", "status" ]
   end

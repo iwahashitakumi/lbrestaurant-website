f005e0930ab8301e6ece801bd4843a29607a1ace
岩橋拓海 <minton@kin.local>
2023-12-17 18:59:51 +0900

    update/job_entryモデルのstrongparametersメソッドをprivateで作成

--- app/controllers/admins/job_entries_controller.rb
+++ app/controllers/admins/job_entries_controller.rb
@@ -1,2 +1,9 @@
 class Admins::JobEntriesController < Admins::ApplicationController
+
+
+  private
+
+  def job_entry_params
+    params.require(:job_entry).permit(:recruit_type, :name, :name_kana, :phone_number, :email, :birth_date, :sex, :city_name, :address, :status, :prefecture_id).merge(update_by: current_admin.id)
+  end
 end

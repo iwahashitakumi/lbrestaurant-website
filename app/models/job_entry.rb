d16b27b136fa4655f9752964db20520b4ae60ffd
岩橋拓海 <minton@kin.local>
2023-12-17 23:43:11 +0900

    update/job_enrtyモデルの一覧で検索フォームを作成するため、modelにransack用の属性および関連を追加

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -7,4 +7,12 @@ class JobEntry < ApplicationRecord
   enumerize :recruit_type, in: {regular_employee: 0, franchisee: 1}, scope: true, predicates: true
   enumerize :sex, in: {male: 0, female: 1}, scope: true, predicates: true
   enumerize :status, in: {not_started: 0, in_progress: 1, completed: 2}, default: :not_started, scope: true, predicates: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name","city_name", "address", "recruit_type", "sex", "status" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["prefecture"]
+  end
 end

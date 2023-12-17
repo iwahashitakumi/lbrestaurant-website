88ae516c28d8557959734b0cd2d693ca9c2693b2
岩橋拓海 <minton@kin.local>
2023-12-17 18:19:10 +0900

    update/enumerizeを利用してjob_entryモデルに複数の列挙型を追加

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -1,4 +1,10 @@
 class JobEntry < ApplicationRecord
   belongs_to :admin, foreign_key: 'update_by'
   belongs_to :prefecture
+
+  extend Enumerize
+
+  enumerize :recruit_type, in: {regular_employee: 0, franchisee: 1}, scope: true, predicates: true
+  enumerize :sex, in: {male: 0, female: 1}, scope: true, predicates: true
+  enumerize :status, in: {not_started: 0, in_progress: 1, completed: 2}, default: :not_started, scope: true, predicates: true
 end

742ed3d411e7ed68a8662d412c61d655471d5f6a
岩橋拓海 <minton@kin.local>
2023-12-21 02:07:18 +0900

    add/公開側のお知らせ詳細画面とcontrollerでshowメソッドを作成

--- app/controllers/public/news_controller.rb
+++ app/controllers/public/news_controller.rb
@@ -1,5 +1,9 @@
 class Public::NewsController < Public::ApplicationController
   def index
-    @news = News.page(params[:page]).per(6)
+    @news = News.page(params[:page]).per(4)
+  end
+
+  def show
+    @news = News.find(params[:id])
   end
 end

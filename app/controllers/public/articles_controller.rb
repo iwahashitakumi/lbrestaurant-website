6ffe7496cdb5e85d23e2336059a846dace1e3613
岩橋拓海 <minton@kin.local>
2023-12-26 16:38:20 +0900

    add/公開側のarticleモデルの詳細画面の作成と、controllerのshowメソッドを作成

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -2,4 +2,8 @@ class Public::ArticlesController < Public::ApplicationController
   def index
     @articles = Article.with_status(:published).page(params[:page]).per(6)
   end
+
+  def show
+    @article = Article.with_status(:published).find(params[:id])
+  end
 end

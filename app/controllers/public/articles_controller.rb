678406c28a1d4437689b28e2713b801bd9bc5032
岩橋拓海 <minton@kin.local>
2023-12-28 13:48:32 +0900

    update/公開側のブログ画面のカテゴリーごとに文字色を変化させる方法をhelperで作成していたのを直接html.erbでcssのクラスを指定するように変更

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -1,9 +1,26 @@
 class Public::ArticlesController < Public::ApplicationController
   def index
     @articles = Article.with_status(:published).page(params[:page]).per(6)
+    @category_css_class = category_css_class(@articles.category)
   end
 
   def show
     @article = Article.with_status(:published).find(params[:id])
+    @category_css_class = category_css_class(@article.category)
+  end
+
+  private
+
+  def category_css_class(category)
+    case category
+    when :event
+      'event-category'
+    when :company_trip
+      'company-trip-category'
+    when :staff_introduction
+      'staff-introduction-category'
+    when :other
+      'other-category'
+    end
   end
 end

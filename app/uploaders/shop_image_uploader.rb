f49893aa6962e70ae1fa84ee50868a60a4b4d3f9
岩橋拓海 <minton@kin.local>
2024-01-06 00:12:19 +0900

    fix/誤ってshop_image_uploader.rbからstorage :fogを削除してしまったため、訂正

--- app/uploaders/shop_image_uploader.rb
+++ app/uploaders/shop_image_uploader.rb
@@ -5,7 +5,7 @@ class ShopImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?    
-    storage :
+    storage :fog
   else
     storage :file
   end

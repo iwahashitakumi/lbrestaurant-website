4f6343cd3e166af8d70385b908ffc37576346ff7
岩橋拓海 <minton@kin.local>
2024-01-10 05:26:57 +0900

    add/レスポンシブな公開側の問い合わせ確認画面を作成しcontrollerでconfirmメソッドを作成しcreateメソッドで確認画面から修正ボタンを押した際、renderが実行されるように設定

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,2 +1,32 @@
 class Public::ContactsController < Public::ApplicationController
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    begin
+      if params[:back]
+        render "new"
+      else
+        @contact.save!
+        redirect_to complete_contacts_path
+      end
+    rescue
+      render "new"
+    end
+  end
+
+  def confirm
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render "new"
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:contact_type, :message, :name, :name_kana, :email, :phone_number, :callback_requested, :contact_method)
+  end
 end

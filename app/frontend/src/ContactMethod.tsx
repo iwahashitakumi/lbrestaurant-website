3ed9db00500b008b2f8a450f92892d6ec4c0c6cd
岩橋拓海 <minton@kin.local>
2024-01-10 20:41:59 +0900

    update/thの方はrailsに任せてtdの中身だけreact で書くように変更し、useEffctを用いてrenderがあった際値を保持するように、またlabelを日本語化するように変更

--- app/frontend/src/ContactMethod.tsx
+++ app/frontend/src/ContactMethod.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface ContactMethodProps {
   contactMethodValuesCollect: string[];
+  contactMethod: string; 
 }
 
 const ContactMethod: React.FC<ContactMethodProps> = (props) => {
-  const { contactMethodValuesCollect } = props;
+  const { contactMethodValuesCollect, contactMethod } = props;
+  const [selectedOption, setSelectedOption] = useState<string | null>(contactMethod || null);
 
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  useEffect(() => {
+    setSelectedOption(contactMethod || null);
+  }, [contactMethod]);
 
   const handleOptionChange = (value: string) => {
     setSelectedOption(value);
@@ -19,32 +23,24 @@ const ContactMethod: React.FC<ContactMethodProps> = (props) => {
 
   return (
     <React.Fragment>
-      <th className="col-md-4">
-        <div className="contact-label">
-          連絡方法
-          <span className="badge rounded-pill text-bg-success">任意</span>
-        </div>
-      </th>
-      <td className="col-md-8">
-        <div className="contact-form">
-          {contactMethodValuesCollect.map((option: string, index: number) => (
-            <div key={option} className="form-check contact-as-radio">
-              <input
-                type="radio"
-                id={option}
-                name={`contact_method_${index}`}
-                value={option}
-                checked={selectedOption === option}
-                onChange={() => handleOptionChange(option)}
-                className="form-check-input"
-              />
-              <label htmlFor={option} className="form-check-label">
-                {option}
-              </label>
-            </div>
-          ))}
-        </div>
-
+      <div className="contact-form">
+        {contactMethodValuesCollect.map((option: string, index: number) => (
+          <div key={option} className="form-check contact-as-radio">
+            <input
+              type="radio"
+              id={option}
+              name="contact[contact_method]"
+              value={option}
+              checked={selectedOption === option}
+              onChange={() => handleOptionChange(option)}
+              className="form-check-input"
+            />
+            <label htmlFor={option} className="form-check-label" lang="ja">
+              {option === 'email' ? "メールアドレス" : option === 'phone_number' ? "電話番号" : option}
+            </label>
+          </div>
+        ))}
+      
         {selectedOption && (
           <div>
             <a className="text-decoration-underline" type="button" onClick={handleClearSelection}>
@@ -52,8 +48,8 @@ const ContactMethod: React.FC<ContactMethodProps> = (props) => {
             </a>
           </div>
         )}
-      </td>
-      </React.Fragment>
+      </div>
+    </React.Fragment>
   );
 };
 

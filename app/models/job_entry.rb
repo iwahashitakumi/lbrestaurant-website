9c056fb53de85c4ba52850708449d2de0d5a837d
岩橋拓海 <minton@kin.local>
2024-01-12 04:29:58 +0900

    update//job_entryモデルのvalidationを追加

--- app/models/job_entry.rb
+++ app/models/job_entry.rb
@@ -8,6 +8,20 @@ class JobEntry < ApplicationRecord
   enumerize :sex, in: {male: 1, female: 2}, scope: true, predicates: true
   enumerize :status, in: {not_started: 1, in_progress: 2, completed: 3}, default: :not_started, scope: true, predicates: true
 
+
+  validates :recruit_type, presence: true
+  validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :sex, presence: true
+  validates :birth_date, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  VALID_PHONE_REGEX = /\A0[-\d]{11,12}\z/
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :prefecture_id, presence: true
+  validates :city_name, presence: true
+  validates :address, presence: true
+  
   scope :search_by_recruit_type, ->(type) { where(recruit_type: type) }
   scope :search_by_sex, ->(method) { where(sex: method) }
   scope :search_by_status, ->(status) { where(status: status) }

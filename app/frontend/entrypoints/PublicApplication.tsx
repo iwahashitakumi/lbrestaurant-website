6c03eaf1675a9fb2a402a79b82fd231c46c40058
岩橋拓海 <minton@kin.local>
2024-01-16 20:51:04 +0900

    update/公開側でreactを使用すると、seo的側面からデメリットが多いため、railsで作成するように変更し、scssを利用して同じような機能を作成

--- app/frontend/entrypoints/PublicApplication.tsx
+++ app/frontend/entrypoints/PublicApplication.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import {useState} from 'react';
 import {rewrap} from "@sonicgarden/rewrap";
 import ContactMethod from '../src/ContactMethod';
-import PublicHeader from '../src/PublicHeader'
 
 
-const element = document.getElementById('header');
+
 const App: React.FC = (props: any) => {
   React.useEffect(() => {
     rewrap('contactmethod-component', ContactMethod, true);
   }, []);
 
   return <>
-    <PublicHeader
-      RootPath="/"
-      CompaniesPath="/companies"
-      NewsPath="/news"
-      ArticlesPath="/articles"
-      ShopsPath="/shops"
-      JobEntriesPath="/job_entries"
-      JobEntriesRecruitmentPath="/job_entries/recruitment_info"
-      JobEntriesFaqPath="/job_entries/faq"
-      ContactsNewPath="/contacts/new"
-      ControllerName={props.ControllerName}
-      ActionName={props.ActionName}
-    ></PublicHeader>
     <main>
       {props.children}
     </main>

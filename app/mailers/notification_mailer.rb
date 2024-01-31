6d5a3f7a11ba1fbcdb2d5bf1bced143009b48cf0
岩橋拓海 <minton@kin.local>
2024-01-31 20:44:01 +0900

    update/notification_mailerの可読性を上げるため、defaultメソッドを利用し、ccではなく全てのメースアドレスをtoで表示

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,15 @@
 class NotificationMailer < ApplicationMailer
+  default to: -> { [ENV['ADMIN_EMAIL'], ENV['ADMIN_INFO_EMAIL'], ENV['ADMIN_OFFICER_EMAIL']].compact }
+
   def contact_notification(contact)
     @contact = contact
     @contact_show_url = admins_contact_url(@contact)
-    mail(to: ENV['ADMIN_EMAIL'], cc: [ENV['ADMIN_INFO_EMAIL'], ENV['ADMIN_OFFICER_EMAIL']].compact, subject: '新しいお問い合わせがあります')
+    mail(subject: '新しいお問い合わせがあります')
   end
 
   def job_entry_notification(job_entry)
     @job_entry = job_entry
     @job_entry_show_url = admins_job_entry_url(@job_entry)
-    mail(to: ENV['ADMIN_EMAIL'], cc: [ENV['ADMIN_INFO_EMAIL'], ENV['ADMIN_OFFICER_EMAIL']].compact, subject: '新しい採用応募があります')
+    mail(subject: '新しい採用応募があります')
   end
 end

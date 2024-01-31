6384ad4ead6c042caecaa44f75553c0a3fac9ce0
岩橋拓海 <minton@kin.local>
2024-01-31 20:27:39 +0900

    update/mailerのtextではリンクを直接記述できないので、linkを変数に詰めてテキストとして表示する

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,13 @@
 class NotificationMailer < ApplicationMailer
   def contact_notification(contact)
     @contact = contact
+    @contact_show_url = admins_contact_url(@contact)
     mail(to: ENV['ADMIN_EMAIL'], cc: [ENV['ADMIN_INFO_EMAIL'], ENV['ADMIN_OFFICER_EMAIL']].compact, subject: '新しいお問い合わせがあります')
   end
 
   def job_entry_notification(job_entry)
     @job_entry = job_entry
+    @job_entry_show_url = admins_job_entry_url(@job_entry)
     mail(to: ENV['ADMIN_EMAIL'], cc: [ENV['ADMIN_INFO_EMAIL'], ENV['ADMIN_OFFICER_EMAIL']].compact, subject: '新しい採用応募があります')
   end
 end

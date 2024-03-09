828495380a7c7074ece598908cfdab5227cf9cca
岩橋拓海 <minton@kin.local>
2024-03-09 21:27:36 +0900

    update/公開側のnews画面をデザイナー様のデザインに合わせて変更、newsモデルとarticleモデルを一緒に表示するように変更(tabの切り替えなど改善点あり、お知らせのデザインはまだ作成されてないので仮のデザイン

--- app/controllers/public/news_controller.rb
+++ app/controllers/public/news_controller.rb
@@ -1,9 +1,11 @@
 class Public::NewsController < Public::ApplicationController
   def index
-    @news = News.with_status(:published).page(params[:page]).per(4)
+    @news = News.with_status(:published).page(params[:page]).per(5)
+    @articles = Article.with_status(:published).page(params[:page]).per(5)
   end
 
   def show
     @news = News.with_status(:published).find(params[:id])
+    @article = Article.with_status(:published).find(params[:id])
   end
 end

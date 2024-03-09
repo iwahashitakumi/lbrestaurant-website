00833f3591869ba703edf8f4fcc0f93fe9755c6b
岩橋拓海 <minton@kin.local>
2024-03-09 21:35:26 +0900

    update/公開側のnews一覧画面でkaminari-gemで作成したpagenateをで二つ利用するためそれぞれparam_nameオプションを利用してpaginationに使用するパラメータのkeyを変更することで、それぞれページを変えれるように変更

--- app/controllers/public/news_controller.rb
+++ app/controllers/public/news_controller.rb
@@ -1,7 +1,7 @@
 class Public::NewsController < Public::ApplicationController
   def index
-    @news = News.with_status(:published).page(params[:page]).per(5)
-    @articles = Article.with_status(:published).page(params[:page]).per(5)
+    @news = News.with_status(:published).page(params[:news_page]).per(5)
+    @articles = Article.with_status(:published).page(params[:article_page]).per(5)
   end
 
   def show

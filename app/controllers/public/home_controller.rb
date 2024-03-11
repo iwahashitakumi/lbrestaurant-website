211a844a3e5e352f0e886e350df25586d0a5c49c
岩橋拓海 <minton@kin.local>
2024-03-11 20:29:27 +0900

    update/公開側のトップページで表示している最新のお知らせとブログ5件が公開されているもの以外も表示されていたため、公開されているものだけに変更

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -8,18 +8,18 @@ class Public::HomeController < Public::ApplicationController
   private
 
   def load_news
-    @latest_news = News.order(created_at: :desc).first
-    @second_latest_news = News.order(created_at: :desc).second
-    @third_latest_news = News.order(created_at: :desc).third
-    @fourth_latest_news = News.order(created_at: :desc).fourth
-    @fifth_latest_news = News.order(created_at: :desc).fifth
+    @latest_news = News.with_status(:published).order(created_at: :desc).first
+    @second_latest_news = News.with_status(:published).order(created_at: :desc).second
+    @third_latest_news = News.with_status(:published).order(created_at: :desc).third
+    @fourth_latest_news = News.with_status(:published).order(created_at: :desc).fourth
+    @fifth_latest_news = News.with_status(:published).order(created_at: :desc).fifth
   end
 
   def load_articles
-    @latest_article = Article.order(created_at: :desc).first
-    @second_latest_article = Article.order(created_at: :desc).second
-    @third_latest_article = Article.order(created_at: :desc).third
-    @fourth_latest_article = Article.order(created_at: :desc).fourth
-    @fifth_latest_article = Article.order(created_at: :desc).fifth
+    @latest_article = Article.with_status(:published).order(created_at: :desc).first
+    @second_latest_article = Article.with_status(:published).order(created_at: :desc).second
+    @third_latest_article = Article.with_status(:published).order(created_at: :desc).third
+    @fourth_latest_article = Article.with_status(:published).order(created_at: :desc).fourth
+    @fifth_latest_article = Article.with_status(:published).order(created_at: :desc).fifth
   end
 end

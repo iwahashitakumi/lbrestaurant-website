7a260a49368f44d4aad1ae75a486ca219e1aa39b
岩橋拓海 <minton@kin.local>
2024-03-16 18:53:40 +0900

    update/エラーメッセージにアイコンを表示するように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
   def show_error_messages(object, attribute)
     if object.errors.full_messages_for(attribute).any?
-      tag.div(class: "text-danger") do
-        tag.ul(class: "mb-0") do
+      content_tag(:div, class: "text-danger") do
+        content_tag(:ul, class: "mb-0") do
           object.errors.full_messages_for(attribute).map do |message|
-            tag.li(message)
+            concat content_tag(:li, content_tag(:i, "", class: "fa-solid fa-circle-xmark error-icon") + message)
           end.join.html_safe
         end
       end
     end
-  end
+  end  
 
   def format_datetime(datetime)
     datetime.strftime("%Y/ %m/ %d /%H:%M")

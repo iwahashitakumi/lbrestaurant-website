bcf8d9a9878a23c637f4957f34e246d854b86460
岩橋拓海 <iwahashitakumi@mbp.local>
2024-03-26 17:13:31 +0900

    update/開発環境のdbをpgに変更

--- db/migrate/20231004035718_create_contacts.rb
+++ db/migrate/20231004035718_create_contacts.rb
@@ -7,7 +7,7 @@ class CreateContacts < ActiveRecord::Migration[7.0]
       t.string :name_kana, default: "", null: false,                comment: "問い合わせ者の名前(カナ)"
       t.string :email, default: "", null: false,                    comment: "問い合わせのメールアドレス"
       t.string :phone_number, default: "", null: false,             comment: "問い合わせの電話番号"
-      t.boolean :callback_requested, null: false,                   comment: "返信の有無"
+      t.integer :callback_requested, null: false,                   comment: "返信の有無"
       t.integer :contact_method, null: false,                       comment: "返信方法"
       t.integer :status, null: false,                               comment: "問い合わせの対応状態"
       t.bigint :update_by, null: true,                             comment: "更新者(外部キー)"

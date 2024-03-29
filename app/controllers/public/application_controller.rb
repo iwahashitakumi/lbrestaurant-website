0739849558763a255019c2d4fb2021bc1899f586
岩橋拓海 <iwahashitakumi@mbp.local>
2024-03-29 22:20:06 +0900

    update/テスト中で未公開である公開側にベーシック認証を作成

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,13 @@
 class Public::ApplicationController < ApplicationController
+  before_action :basic_auth
+  protect_from_forgery with: :exception
+
   layout 'public'
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end

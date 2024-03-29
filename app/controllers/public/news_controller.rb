415427f448d55d73bd2e1b3f0b7b5ca9922794ab
岩橋拓海 <iwahashitakumi@mbp.local>
2024-03-30 01:16:14 +0900

    update/公開側のお知らせの表示をcalendar_dateカラムの最新の日時から降順で表示するように設定

--- app/controllers/public/news_controller.rb
+++ app/controllers/public/news_controller.rb
@@ -1,7 +1,7 @@
 class Public::NewsController < Public::ApplicationController
   def index
     @active_tab = params[:active_tab]
-    @news = News.with_status(:published).page(params[:news_page]).per(5).order(created_at: :desc)
+    @news = News.with_status(:published).order(calendar_date: :desc, created_at: :desc).page(params[:news_page]).per(5)
     @articles = Article.with_status(:published).page(params[:blog_page]).per(5).order(created_at: :desc)
   end
 

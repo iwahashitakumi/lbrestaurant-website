c5d677da708a3b8eae32813ad77afac3b0ddfa69
岩橋拓海 <iwahashitakumi@mbp.local>
2024-03-29 17:47:23 +0900

    update/helperに公開側のseo対策で利用できるmata_tags-gemの基礎設定を導入して共有レイアウトファイルに設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,33 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'LBrestaurant(株式会社)公式サイト(暖簾分け希望者、求職者募集中!)',
+      title: '',
+      reverse: true,
+      charset: 'utf-8',
+      description: '地域密着の飲食店の創出×店長の高収入×店長の人間らしい生活を同時に達成する’ブラックな飲食業界を変える！’・LBrestaurant(株式会社)の公式ホームページ',
+      keywords: '飲食,居酒屋,大阪,焼き鳥,求人,採用',
+      canonical: 'https://lbrestaurant.net/',
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site: 'LBrestaurant(株式会社)公式サイト(暖簾分け希望者、求職者募集中!)',
+        title: '',
+        description: '地域密着の飲食店の創出×店長の高収入×店長の人間らしい生活を同時に達成する’ブラックな飲食業界を変える！’・LBrestaurant(株式会社)の公式ホームページ',
+        type: 'website',
+        url: 'https://lbrestaurant.net/',
+        image: image_url('opg.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('opg.png'),
+      }
+    }
+  end
   def show_error_messages(object, attribute)
     if object.errors.full_messages_for(attribute).any?
       content_tag(:div, class: "text-danger") do

de67804b0e50583e16ce94e396711605e679c1db
岩橋拓海 <iwahashitakumi@mbp-2.local>
2024-07-07 20:59:03 +0900

    修正

--- app/validators/phone_validator.rb
+++ app/validators/phone_validator.rb
@@ -1,6 +1,6 @@
 class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.match(/\A0[-\d]{11,12}\z/)
+    unless value.match(/\A0[-\d]{10,11}\z/)
       record.errors.add(attribute, options[:message] || "の形式が不正です")
     end
   end

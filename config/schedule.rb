543cee67d75761cde20ab7674a2baa89cb68c663
岩橋拓海 <iwahashitakumi@mbp-2.local>
2024-07-07 16:58:28 +0900

    スケジュールの修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
 
+set :path_env, ENV['PATH']
+job_type :runner, "cd :path && PATH=':path_env' bin/rails runner -e :environment ':task' :output"
+job_type :rake,   "cd :path && PATH=':path_env' :environment_variable=:environment bundle exec rake :task --silent :output"
 
 rails_env = ENV['RAILS_ENV'] || :development
 
-
 set :environment, rails_env
-
 
 set :output, "#{Rails.root}/log/cron.log"
 
